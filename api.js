4ea2e42886f583d5c7670ac6144d2e79a5fda412
Alan Stahl <alanst@umich.edu>
2017-01-30 11:54:51 -0500

    Improve error handling

--- api.js
+++ api.js
@@ -3,8 +3,28 @@ Site = function(url) {
     this.json_url = url + '/wp-json';
 }
 
-Site.prototype.getData = function(callback, onError) {
-    $.getJSON(this.json_url).done(callback).fail(onError);
+Site.getResponseErrorMessage = function(response) {
+    if (response.responseJSON) {
+        if (response.responseJSON.code == "rest_no_route") {
+            return "This site uses an older version of Wordpress which does not work with this tool.";
+        }
+    }
+    if (response.status == 404) {
+        return "Site not found";
+    }
+    return "Request failed: " + response.statusText + " (" + response.status + ")";
+}
+
+Site.prototype.getData = function() {
+    var events = $({});
+    $.getJSON(this.json_url)
+        .done(function(data) {
+            events.trigger('done', data);
+        })
+        .fail(function(response) {
+            events.trigger('error', {message: Site.getResponseErrorMessage(response)});
+        });
+    return events;
 }
 
 Site.prototype.getAllPages = function() {
@@ -14,16 +34,13 @@ Site.prototype.getAllPages = function() {
     var page_count = 0;
     var page_data = [];
 
-    var onError = function(e) {
-        events.trigger('error', e);
+    var onError = function(response) {
+        events.trigger('error', {message: Site.getResponseErrorMessage(response)});
     };
 
     var fetchPageGroup = function(page_id) {
-        console.log('fetching page ', page_id)
         $.getJSON(url, {page: page_id, per_page: per_page}, function(data) {
-            console.log('got page ', page_id);
             page_data = page_data.concat(data);
-            console.log(page_data);
             events.trigger('progress', {current: page_id, total: page_count});
             if (page_id < page_count) {
                 fetchPageGroup(page_id + 1);
@@ -35,9 +52,7 @@ Site.prototype.getAllPages = function() {
     };
 
     var first_request = $.getJSON(url, {per_page: per_page}, function(data) {
-        console.log('got first page');
         page_data = data;
-        console.log(page_data);
         page_count = Number(first_request.getResponseHeader('X-WP-TotalPages'));
         events.trigger('progress', {current: 1, total: page_count});
         if (page_count > 1) {
@@ -46,7 +61,7 @@ Site.prototype.getAllPages = function() {
         else {
             events.trigger('done', {pages: page_data});
         }
-    });
+    }).fail(onError);
 
     return events;
 }
--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function generateExport(site) {
-    site.getData(function(data) {
-        $('#page-content h1#site-name').text(data.name);
-    }, handleError);
+    site.getData()
+        .on('done', function(event, data) {
+            $('#page-content h1#site-name').text(data.name);
+        })
+        .on('error', handleError);
 
     $('#load-info').show();
+    $('#load-error').hide();
     updatePageProgress({}, {current: 0, total: 1});
     site.getAllPages()
         .on('progress', updatePageProgress)
@@ -13,17 +16,17 @@ function generateExport(site) {
     $('#site-contents').text('');
 }
 
-function handleError(msg) {
-    console.warn(msg);
+function handleError(event, info) {
+    console.warn(info.message);
+    $('#load-info').hide();
+    $('#load-error').show().find('.alert').text(info.message);
 }
 
 function updatePageProgress(event, info) {
-    console.log(info);
     $('#load-info .progress-bar').width((info.current / info.total * 100) + '%');
 }
 
 function renderPages(event, data) {
-    console.log('renderPages');
     $('#load-info').hide();
     updatePageProgress({}, {current: 0, total: 1});
     $.each(data.pages, function(_, page) {
@@ -39,6 +42,8 @@ jQuery(function($) {
     $('.hidden').hide().removeClass('hidden');
     $('form').on('submit', function(e) {
         e.preventDefault();
-        generateExport(new Site($(this).find('input#url').val()));
+        var url = $(this).find('input#url').val();
+        if (url)
+            generateExport(new Site(url));
     });
 });
