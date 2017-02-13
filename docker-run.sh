f44665e9828c57c9da26be0e8daa418ee9b7666f
Alan Stahl <alanst@umich.edu>
2017-02-13 12:42:48 -0500

    Add nginx js check

--- docker-run.sh
+++ docker-run.sh
@@ -4,5 +4,5 @@ docker run \
     --name nginx-wp-print-tool \
     -p 8000:80 \
     -v "$(pwd)/web":/usr/share/nginx/html:ro \
-    -v "$(pwd)"/nginx/nginx.conf:/etc/nginx/conf:ro \
+    -v "$(pwd)"/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
     -d nginx
---
+++ web/nginx-on.js
@@ -0,0 +1,1 @@
+window.HAS_NGINX = true;
